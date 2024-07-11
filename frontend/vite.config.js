23e9811efcc46010f923a8ef7ab88907b829198b
Samita <samitachaudhary091190@gmail.com>
2024-07-11 09:58:15 +0530

    this is it

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000, // Ensure this matches the port you are expecting
+    port: 3000, // Ensure this matches the port you are expecting this is good
     proxy: {
       '/api': {
         target: 'http://localhost:8004',
